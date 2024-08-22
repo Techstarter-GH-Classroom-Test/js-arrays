5883fb27ad1cb20b37446ac5ec2929af35c7b677
faensi <knobling.hubertu@gmail.com>
2024-08-22 16:03:43 +0200

    Update exerciseArrayBasics.js

--- exerciseArrayBasics.js
+++ exerciseArrayBasics.js
@@ -10,3 +10,8 @@ console.log(arr1); // --> in der Ausgabe der Konsole sieht das Ergebnis wie folg
 console.log(arr2); // --> in der Ausgabe der Konsole sieht das Ergebnis wie folgt aus: []
 console.log(arr3); // --> in der Ausgabe der Konsole sieht das Ergebnis wie folgt aus: []
 console.log(arr4); // --> in der Ausgabe der Konsole sieht das Ergebnis wie folgt aus: []
+
+
+// Folgenden Code nicht verändern!
+// Exportiere die Arrays
+module.exports = { arr1, arr2, arr3, arr4 };
