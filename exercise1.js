55817c6d35dcae65354f62096f28db1fc1757e6e
faensi <knobling.hubertu@gmail.com>
2024-08-22 14:12:23 +0200

    test different function approach

--- exercise1.js
+++ exercise1.js
@@ -1,7 +1,11 @@
 // exercise1.js
-const numbers = [1, 2, 3, 4, 5];
-numbers.reverse(); // -> reverse array order
-console.log(numbers); // [5, 4, 3, 2, 1]
-
-numbers.reverse();
-console.log(numbers); // [1, 2, 3, 4, 5]
+function reverseArray(numbers) {
+    numbers.reverse(); // -> reverse array order
+    console.log(numbers); // [5, 4, 3, 2, 1]
+  
+    numbers.reverse();
+    console.log(numbers); // [1, 2, 3, 4, 5]
+  }
+  
+  module.exports = reverseArray;
+  
