9bf109ee5ee732ad1b88a3820d6fc2e2201a5886
faensi <knobling.hubertu@gmail.com>
2024-08-22 14:18:55 +0200

    make function static

--- musterloesung.exercise1.js
+++ musterloesung.exercise1.js
@@ -1,19 +1,11 @@
 // exercise1.js
-const numbers = [1,2,3,4,5]
-
-function reverseArray() {
-
-
+function reverseArray(numbers) {
     numbers.reverse(); // -> reverse array order
     console.log(numbers); // [5, 4, 3, 2, 1]
   
     numbers.reverse();
     console.log(numbers); // [1, 2, 3, 4, 5]
-
-
-
 }
-
-reverseArray();
-
+  
 module.exports = reverseArray;
+  
--- tests/exercise1.test.js
+++ tests/exercise1.test.js
@@ -15,8 +15,7 @@ describe('reverseArray function', () => {
   });
 
   it('should use the .reverse() method on the array', () => {
-    const numbers = [1, 2, 3, 4, 5];
-    reverseArray(numbers); // Call the student's function
+    reverseArray(); // Call the student's function
 
     // Check if the .reverse() method was called
     expect(spyReverse).toHaveBeenCalled();
@@ -25,9 +24,7 @@ describe('reverseArray function', () => {
 
   it('should not use any other array methods', () => {
     const spySort = jest.spyOn(Array.prototype, 'sort'); // Example of another method to check
-
-    const numbers = [1, 2, 3, 4, 5];
-    reverseArray(numbers); // Call the student's function
+    reverseArray(); // Call the student's function
 
     expect(spySort).not.toHaveBeenCalled();
 
