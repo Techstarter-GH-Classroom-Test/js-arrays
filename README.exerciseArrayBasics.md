fddf5461260797c0be51eb89bef274bc547de1b5
faensi <knobling.hubertu@gmail.com>
2024-08-22 16:22:35 +0200

    add Aufgabenstellung

---
+++ README.exerciseArrayBasics.md
@@ -0,0 +1,15 @@
+# Array Syntax Basics
+
+## Ziel
+
+In dieser Übung wirst du dich mit der Erzeugung von Arrays in JavaScript beschäftigen. Du sollst herausfinden, wie man Arrays auf verschiedene Arten erstellt und welche Ausgabe dies in der Konsole erzeugt.
+
+## Aufgabe
+
+1. **Entferne die Kommentare:** Deine Aufgabe ist es, die Kommentare im bereitgestellten Code zu entfernen und den Code auszuführen. Achte darauf, den Code nicht zu verändern – nur die Kommentare sollten entfernt werden.
+
+2. **Führe den Code aus:** Teste den Code in deiner Entwicklungsumgebung oder Konsole und beobachte die Ausgaben, die du erhältst.
+
+## Beispiel-Code
+
+Der Code, den du bearbeiten sollst, ist in der Datei ```exerciseArrayBasics.js```.
--- exerciseArrayBasics.js
+++ exerciseArrayBasics.js
@@ -1,17 +1,26 @@
-// syntax
-// Entferne die Kommentare:
+// Syntax
+// Entferne die Kommentare und probiere es aus:
 
+/***********
 const arr1 = Array();
-let arr2 = new Array();
-const arr3 = [];
-let arr4 = [];
-
 console.log(arr1); // --> in der Ausgabe der Konsole sieht das Ergebnis wie folgt aus: []
+***********/
+
+/***********
+let arr2 = new Array();
 console.log(arr2); // --> in der Ausgabe der Konsole sieht das Ergebnis wie folgt aus: []
+***********/
+
+/***********
+const arr3 = [];
 console.log(arr3); // --> in der Ausgabe der Konsole sieht das Ergebnis wie folgt aus: []
+***********/
+
+/***********
+let arr4 = [];
 console.log(arr4); // --> in der Ausgabe der Konsole sieht das Ergebnis wie folgt aus: []
+***********/
 
 
 // Folgenden Code nicht verändern!
-// Exportiere die Arrays
 module.exports = { arr1, arr2, arr3, arr4 };
